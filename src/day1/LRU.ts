9d30e749bad49c8c14eae241f99170ac760d6e8e
Jacob Gardner <Jacob.gardner@bjss.com>
2023-10-23 22:39:30 +0100

    feat: write out structure of LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,56 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
+}
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
     }
 
     update(key: K, value: V): void {
+        let node = this.lookup.get(key);
+        if (!node) {
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimCache();
+        } else {
+            this.detach(node);
+            this.prepend(node);
+        }
+    }
 
-}
     get(key: K): V | undefined {
+        const node = this.lookup.get(key);
+        if (!node) {
+            return undefined;
+        }
 
-}
+        this.detach(node);
+        this.prepend(node);
+
+        return node.value;
+    }
+
+    private detach(node: Node<V>) {}
+
+    private prepend(node: Node<V>) {}
+
+    private trimCache(): void {}
 }
